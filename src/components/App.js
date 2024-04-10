49b761fa54cc936a769a3fddc70b0223b1f2ce35
droopyantidote <sanketshikhar18@gmail.com>
2024-04-10 12:01:16 +0530

    third commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 
 import React from "react";
-import ClickCounter from "./ClickCounter";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
          <h1>Welcome to React App</h1>
-      <ClickCounter />
+      
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import ClickCounter from "./components/ClickCounter"; 
 
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(  <React.StrictMode>
+    <App>
+      <ClickCounter /> 
+    </App>
+  </React.StrictMode>,
+    document.getElementById("root"));
